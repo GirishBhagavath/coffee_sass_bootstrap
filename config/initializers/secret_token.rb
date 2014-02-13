3e4666d5adc00154739d743592f18d90fdfd7cc9
Girish Bhagavath <girish.bhagavath@in.v2solutions.com.com>
2014-02-13 18:59:14 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeSassBootstrap::Application.config.secret_token = 'bae8f1b2357c61f76f078d99068f7acc6c55fde7b4e113751150c285ca214d5f4ec0d43c34a49593f11640b935fa35622d4a84c99b8543a10df820e455313abf'
